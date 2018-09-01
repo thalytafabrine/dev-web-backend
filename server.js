6d03443486dfa689ba8fada2398a87e22f418632
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2018-09-01 17:17:47 -0300

    Adding morgan

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 const disciplina = require('./routes/disciplinaRoutes')
 const listaEstudo = require('./routes/listaEstudoRoutes')
+
+// create a write stream (in append mode)
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+app.use(morgan('combined', {stream: accessLogStream}))
+
 app.use('/disciplina', disciplina)
 app.use('/listaEstudo', listaEstudo)
-
 
 // ROTAS QUE AINDA DEVEM SER CRIADAS CORRETAMENTE
 
