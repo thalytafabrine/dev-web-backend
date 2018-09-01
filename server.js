d5263174c2ecaeab080b0bdda3363819ba6ce2ea
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2018-09-01 16:02:37 -0300

    Pensando em novas URIs do sistema

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const express = require('express'),
     Disciplina = require('./models/disciplinaModel'),
     bodyParser = require('body-parser');
 
-// criar constantes para as URIs
-// funções que vão fazer as requisições - disciplina done
-
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Disciplinabd', {useNewUrlParser: true});
 
@@ -21,6 +18,19 @@ routes(app);
  * Rotas para a atividade - falta criar
  * app.get('/disciplina/:id/atividade/:id', (req, res) => res.send("Atividade x da disciplina escolhida."))
  * app.put('/disciplina/:id/atividade/:id', (req, res) => res.send("Submissão da atividade x."))
+ * 
+ * Rota geral para visão inicial do sistema
+ * app.get('/', (req, res) => res.send("Oi você!"))
+ * 
+ * Rota para perfil
+ * app.get('/:username', (req, res) => res.send("Esse é o seu perfil!"))
+ * 
+ * Rotas para lista de estudo - sem disciplina
+ * app.get('/listaEstudo', (req, res) => res.send("Listas de estudo que você criou"))
+ * app.post('/listaEstudo', (req, res) => res.send("Nova lista criada com sucesso!"))
+ * app.get('/listaEstudo/:idLista', (req, res) => res.send("Essa é sua lista de estudo."))
+ * app.put('/listaEstudo/:idLista', (req, res) => res.send("Lista atualizada com sucesso."))
+ * app.delete('/listaEstudo/:idLista', (req, res) => res.send("Lista excluída com sucesso."))
  */
 
 app.listen(port, () => console.log('Example app RESTful API server started on port ' + port + '!'))
