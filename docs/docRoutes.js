e822e0b672a7c6f61943159692f5acba11066ba2
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2018-09-16 23:14:47 -0300

    Adding swagger

---
+++ docs/docRoutes.js
@@ -0,0 +1,8 @@
+const swaggerUi = require('swagger-ui-express'),
+    swaggerSpec = require('./swagger.js');
+
+module.exports = (app) => {
+
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+};
